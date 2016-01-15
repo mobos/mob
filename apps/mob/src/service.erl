da551629ee1415be0e41eaca7fe8aa0ad815d4bc
0xNaN <enzodicicco@gmail.com>
2016-01-15 12:57:02 +0100

    fixing indendation

--- apps/mob/src/service.erl
+++ apps/mob/src/service.erl
@@ -6,20 +6,20 @@
 -export([parse/1]).
 
 -export([init/1,
-         spawned/2,
-         handle_event/3,
-         handle_sync_event/4,
-         handle_info/3,
-         terminate/3,
-         code_change/4]).
+     spawned/2,
+     handle_event/3,
+     handle_sync_event/4,
+     handle_info/3,
+     terminate/3,
+     code_change/4]).
 
 -include("service.hrl").
 
 parse(Service) ->
     BinaryService = list_to_binary(Service),
     case jsx:is_json(BinaryService) of
-        false -> {error, format_error};
-        true ->  {ok, json_to_service(BinaryService)}
+    false -> {error, format_error};
+    true ->  {ok, json_to_service(BinaryService)}
     end.
 
 json_to_service(BinaryService) ->
@@ -33,31 +33,31 @@ json_to_service(BinaryService) ->
     }.
 
 spawn(Service = #service{name = ServiceName}) ->
-        gen_fsm:start({local, ServiceName}, ?MODULE, [Service], []).
+    gen_fsm:start({local, ServiceName}, ?MODULE, [Service], []).
 
 %% gen_fsm callbacks
 
 init([Service]) ->
-        {ok, spawned, Service}.
+    {ok, spawned, Service}.
 
 spawned(_Event, State) ->
-        {next_state, spawned, State}.
+    {next_state, spawned, State}.
 
 handle_event(_Event, StateName, State) ->
-        {next_state, StateName, State}.
+    {next_state, StateName, State}.
 
 handle_sync_event(_Event, _From, StateName, State) ->
-        Reply = ok,
-        {reply, Reply, StateName, State}.
+    Reply = ok,
+    {reply, Reply, StateName, State}.
 
 handle_info(_Info, StateName, State) ->
-        {next_state, StateName, State}.
+    {next_state, StateName, State}.
 
 terminate(_Reason, _StateName, _State) ->
-        ok.
+    ok.
 
 code_change(_OldVsn, StateName, State, _Extra) ->
-        {ok, StateName, State}.
+    {ok, StateName, State}.
 
 
 -ifdef(TEST).
