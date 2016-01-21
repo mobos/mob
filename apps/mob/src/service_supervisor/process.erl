031e3797cd3aa552d08bf9f66220db273c9c8b65
0xNaN <enzodicicco@gmail.com>
2016-01-21 11:14:08 +0100

    adding process:status()

--- apps/mob/src/service_supervisor/process.erl
+++ apps/mob/src/service_supervisor/process.erl
@@ -2,6 +2,7 @@
 
 -export([exec/1]).
 -export([stop/1]).
+-export([status/1]).
 
 exec(Command) ->
     {ok, Pid, OSPid} = exec:run(Command, [monitor]),
@@ -10,3 +11,15 @@ exec(Command) ->
 stop(ProcessPid) ->
     %%XXX what if doesn't exit?
     exec:stop(ProcessPid).
+
+status(normal) -> {exit_code, 0};
+status({exit_status, Exit}) ->
+    case exec:status(Exit) of
+        {status, E} -> {exit_code, E};
+        {signal, _Signal, _} -> {signal, Exit}
+    end.
+
+-ifdef(TEST).
+-compile([export_all]).
+-include_lib("../../test/process.hrl").
+-endif.
---
+++ apps/mob/test/process.hrl
@@ -0,0 +1,13 @@
+-include_lib("eunit/include/eunit.hrl").
+
+translate_normal_exit_to_zero_exit_code_test() ->
+    ExitInfo = normal,
+    ?assertEqual({exit_code, 0}, process:status(ExitInfo)).
+
+translate_successfully_exit_to_its_exit_code_test() ->
+    ExitInfo = {exit_status, 256},
+    ?assertEqual({exit_code, 1}, process:status(ExitInfo)).
+
+translate_signal_exit_to_signal_code_test() ->
+    ExitInfo = {exit_status, 15},
+    ?assertEqual({signal, 15}, process:status(ExitInfo)).
