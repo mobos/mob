44d417ed866419647bff87d6f881503792c5fbf0
0xNaN <enzodicicco@gmail.com>
2016-05-20 23:18:45 +0200

    highlighting setup/trigger/assert sections

--- apps/mob/test/kbucket.hrl
+++ apps/mob/test/kbucket.hrl
@@ -48,7 +48,9 @@ should_start_a_kbucket_process({KbucketPid, _}) ->
 
 should_create_a_bucket_if_not_exists({KbucketPid, _}) ->
     PeerContact = {self(), 2#1111},
+
     ok = kbucket:put(KbucketPid, PeerContact),
+
     [?_assertEqual([PeerContact], kbucket:get(KbucketPid, 1))].
 
 should_returns_an_empty_list_for_an_unknown_distance({KbucketPid, _}) ->
@@ -80,12 +82,12 @@ ping_the_least_seen_contact_when_a_bucket_is_full_and_remove_if_doesnt_respond({
     SixPeerContact   = {self(), 2#1011},
     SevenPeerContact = {self(), 2#1010},
     put_contacts(KbucketPid, [FourPeerContact, FivePeerContact, SixPeerContact]),
-
     meck:expect(peer, check_link,  ?two_any_args(?return(ko))),
+
     ok = kbucket:put(KbucketPid, SevenPeerContact),
 
-    [?_assertEqual([FivePeerContact, SixPeerContact, SevenPeerContact],
-                   kbucket:get(KbucketPid, 2))].
+    ExpectedBucket = [FivePeerContact, SixPeerContact, SevenPeerContact],
+    [?_assertEqual(ExpectedBucket, kbucket:get(KbucketPid, 2))].
 
 ping_the_least_seen_contact_when_a_bucket_is_full_and_mantain_it_if_respond({KbucketPid, _}) ->
     FourPeerContact  = peer:start(2#1001, 3, ?ALPHA),
@@ -93,12 +95,12 @@ ping_the_least_seen_contact_when_a_bucket_is_full_and_mantain_it_if_respond({Kbu
     SixPeerContact   = {self(), 2#1011},
     SevenPeerContact = {self(), 2#1010},
     put_contacts(KbucketPid, [FourPeerContact, FivePeerContact, SixPeerContact]),
-
     meck:expect(peer, check_link, ?two_any_args(?return(ok))),
+
     ok = kbucket:put(KbucketPid, SevenPeerContact),
 
-    [?_assertEqual([FourPeerContact, FivePeerContact, SixPeerContact],
-                   kbucket:get(KbucketPid, 2))].
+    ExpectedBucket = [FourPeerContact, FivePeerContact, SixPeerContact],
+    [?_assertEqual(ExpectedBucket, kbucket:get(KbucketPid, 2))].
 
 should_returns_up_to_k_contacts_closest_to_a_key({KbucketPid, _}) ->
     ThreePeerContact  = {self(), 2#1001},
@@ -106,8 +108,9 @@ should_returns_up_to_k_contacts_closest_to_a_key({KbucketPid, _}) ->
     OnePeerContact    = {self(), 2#1011},
     put_contacts(KbucketPid, [OnePeerContact, TwoPeerContact, ThreePeerContact]),
 
-    [?_assertEqual([OnePeerContact, TwoPeerContact, ThreePeerContact],
-                   kbucket:closest_contacts(KbucketPid, 2#1010))].
+    ClosestContacts = kbucket:closest_contacts(KbucketPid, 2#1010),
+
+    [?_assertEqual([OnePeerContact, TwoPeerContact, ThreePeerContact], ClosestContacts)].
 
 should_fill_the_results_with_other_contacts_if_the_closest_bucket_is_not_full({KbucketPid, _}) ->
     FivePeerContact   = {self(), 2#1111},
@@ -115,15 +118,15 @@ should_fill_the_results_with_other_contacts_if_the_closest_bucket_is_not_full({K
     TwoPeerContact    = {self(), 2#1000},
     put_contacts(KbucketPid, [TwoPeerContact, ThreePeerContact, FivePeerContact]),
 
-    [?_assertEqual([TwoPeerContact, ThreePeerContact, FivePeerContact],
-                   kbucket:closest_contacts(KbucketPid, 2#1010))].
+    ClosestContacts = kbucket:closest_contacts(KbucketPid, 2#1010),
+
+    [?_assertEqual([TwoPeerContact, ThreePeerContact, FivePeerContact], ClosestContacts)].
 
 should_search_a_key_within_each_bucket_and_refresh_its_content({KbucketPid, Peer}) ->
     ZeroBucketContact  = {self(), 12},
     OneBucketContact   = {self(), 15},
     TwoBucketContact   = {self(),  9},
     ThreeBucketContact = {self(),  5},
-
     AllContacts = [ZeroBucketContact, OneBucketContact, TwoBucketContact, ThreeBucketContact],
     meck:expect(peer, iterative_find_peers, fun(_, Key) ->
                                                 case Key of
@@ -135,10 +138,11 @@ should_search_a_key_within_each_bucket_and_refresh_its_content({KbucketPid, Peer
                                             end),
 
     kbucket:refresh(KbucketPid),
+
     timer:sleep(50),
-    [?_assertEqual([ZeroBucketContact], kbucket:get(KbucketPid, 0)),
-     ?_assertEqual([OneBucketContact], kbucket:get(KbucketPid, 1)),
-     ?_assertEqual([TwoBucketContact], kbucket:get(KbucketPid, 2)),
+    [?_assertEqual([ZeroBucketContact],  kbucket:get(KbucketPid, 0)),
+     ?_assertEqual([OneBucketContact],   kbucket:get(KbucketPid, 1)),
+     ?_assertEqual([TwoBucketContact],   kbucket:get(KbucketPid, 2)),
      ?_assertEqual([ThreeBucketContact], kbucket:get(KbucketPid, 3))].
 
 put_contacts(KbucketPid, []) ->
