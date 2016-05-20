a456cb310085d3d8bb12993fb13936f88f239c24
0xNaN <enzodicicco@gmail.com>
2016-05-20 23:19:09 +0200

    removing duplication of fake peers
    Using a fixed PID ("<0.255.255>" in this case) rather than
    self() allows to define fake peers as macro.
    Using self() can leads to problems inside the assert() since
    it is evaluated in another process.

--- apps/mob/test/kbucket.hrl
+++ apps/mob/test/kbucket.hrl
@@ -5,6 +5,28 @@
 -define(ID_LENGTH, 4).
 -define(ALPHA, 3).
 
+% build a fake peer with the specified ID and fixed (dummy) PID
+-define(FAKE_PEER(ID), {list_to_pid("<0.255.255>"), ID}).
+
+% fake peers.
+% the IDs are such that their Bucket Index are those specified
+% on the prefix of the name (ZERO, ONE, ...) if stored in a
+% peer with ID 2#1101 (?PEER_ID). On the same bucket index, the
+% fake peers are sorted on distance (so _1 is closer than _2)
+%
+%                                           DISTANCE
+-define(ZERO_PEER_1, ?FAKE_PEER(2#1100)).  % 1
+
+-define(ONE_PEER_1,  ?FAKE_PEER(2#1111)).  % 2
+-define(ONE_PEER_2,  ?FAKE_PEER(2#1110)).  % 3
+
+-define(TWO_PEER_1,  ?FAKE_PEER(2#1001)).  % 4
+-define(TWO_PEER_2,  ?FAKE_PEER(2#1000)).  % 5
+-define(TWO_PEER_3,  ?FAKE_PEER(2#1011)).  % 6
+-define(TWO_PEER_4,  ?FAKE_PEER(2#1010)).  % 7
+
+-define(THIRD_PEER_1, ?FAKE_PEER(2#0101)). % 10
+
 start() ->
     meck:new(peer),
     meck:expect(peer, start, fun(Id, Kbucket, Alpha) -> {self(), Id} end),
@@ -47,99 +69,70 @@ should_start_a_kbucket_process({KbucketPid, _}) ->
     [?_assert(erlang:is_pid(KbucketPid))].
 
 should_create_a_bucket_if_not_exists({KbucketPid, _}) ->
-    PeerContact = {self(), 2#1111},
+    ok = kbucket:put(KbucketPid, ?ONE_PEER_1),
 
-    ok = kbucket:put(KbucketPid, PeerContact),
-
-    [?_assertEqual([PeerContact], kbucket:get(KbucketPid, 1))].
+    [?_assertEqual([?ONE_PEER_1], kbucket:get(KbucketPid, 1))].
 
 should_returns_an_empty_list_for_an_unknown_distance({KbucketPid, _}) ->
     [?_assertEqual([], kbucket:get(KbucketPid, 100))].
 
 should_append_a_contacts_with_the_same_bucket_index({KbucketPid, _}) ->
-    PeerContact    = {self(), 2#1111}, % distance of 2 from OwningId
-    AnotherContact = {self(), 2#1110}, % distance of 3 from OwningId
+    put_contacts(KbucketPid, [?ONE_PEER_1, ?ONE_PEER_2]),
 
-    put_contacts(KbucketPid, [PeerContact, AnotherContact]),
-
-    [?_assertEqual([PeerContact, AnotherContact], kbucket:get(KbucketPid, 1))].
+    [?_assertEqual([?ONE_PEER_1, ?ONE_PEER_2], kbucket:get(KbucketPid, 1))].
 
 should_update_an_already_present_contacts({KbucketPid, _}) ->
-    PeerContact    = {self(), 2#1111},
-    AnotherContact = {self(), 2#1110},
-    put_contacts(KbucketPid, [PeerContact, AnotherContact]),
+    put_contacts(KbucketPid, [?ONE_PEER_1, ?ONE_PEER_2]),
 
-    ok = kbucket:put(KbucketPid, PeerContact),
+    ok = kbucket:put(KbucketPid, ?ONE_PEER_1),
 
-    [?_assertEqual([AnotherContact, PeerContact], kbucket:get(KbucketPid, 1))].
+    [?_assertEqual([?ONE_PEER_2, ?ONE_PEER_1], kbucket:get(KbucketPid, 1))].
 
 ping_the_least_seen_contact_when_a_bucket_is_full_and_remove_if_doesnt_respond({KbucketPid, _}) ->
-    % these defines fakes contacts.
-    % the IDs are such that its BucketIndex are the
-    % same if stored in a peer with 2#1101 ID.
-    FourPeerContact  = peer:start(2#1001, 3, ?ALPHA),
-    FivePeerContact  = {self(), 2#1000},
-    SixPeerContact   = {self(), 2#1011},
-    SevenPeerContact = {self(), 2#1010},
-    put_contacts(KbucketPid, [FourPeerContact, FivePeerContact, SixPeerContact]),
     meck:expect(peer, check_link,  ?two_any_args(?return(ko))),
+    put_contacts(KbucketPid, [?TWO_PEER_1, ?TWO_PEER_2, ?TWO_PEER_3]),
 
-    ok = kbucket:put(KbucketPid, SevenPeerContact),
+    ok = kbucket:put(KbucketPid, ?TWO_PEER_4),
 
-    ExpectedBucket = [FivePeerContact, SixPeerContact, SevenPeerContact],
+    ExpectedBucket = [?TWO_PEER_2, ?TWO_PEER_3, ?TWO_PEER_4],
     [?_assertEqual(ExpectedBucket, kbucket:get(KbucketPid, 2))].
 
 ping_the_least_seen_contact_when_a_bucket_is_full_and_mantain_it_if_respond({KbucketPid, _}) ->
-    FourPeerContact  = peer:start(2#1001, 3, ?ALPHA),
-    FivePeerContact  = {self(), 2#1000},
-    SixPeerContact   = {self(), 2#1011},
-    SevenPeerContact = {self(), 2#1010},
-    put_contacts(KbucketPid, [FourPeerContact, FivePeerContact, SixPeerContact]),
+    put_contacts(KbucketPid, [?TWO_PEER_1, ?TWO_PEER_2, ?TWO_PEER_3]),
     meck:expect(peer, check_link, ?two_any_args(?return(ok))),
 
-    ok = kbucket:put(KbucketPid, SevenPeerContact),
+    ok = kbucket:put(KbucketPid, ?TWO_PEER_4),
 
-    ExpectedBucket = [FourPeerContact, FivePeerContact, SixPeerContact],
+    ExpectedBucket = [?TWO_PEER_1, ?TWO_PEER_2, ?TWO_PEER_3],
     [?_assertEqual(ExpectedBucket, kbucket:get(KbucketPid, 2))].
 
 should_returns_up_to_k_contacts_closest_to_a_key({KbucketPid, _}) ->
-    ThreePeerContact  = {self(), 2#1001},
-    TwoPeerContact    = {self(), 2#1000},
-    OnePeerContact    = {self(), 2#1011},
-    put_contacts(KbucketPid, [OnePeerContact, TwoPeerContact, ThreePeerContact]),
+    put_contacts(KbucketPid, [?TWO_PEER_3, ?TWO_PEER_2, ?TWO_PEER_1]),
 
-    ClosestContacts = kbucket:closest_contacts(KbucketPid, 2#1010),
+    ClosestContacts = kbucket:closest_contacts(KbucketPid, 2#1101),
 
-    [?_assertEqual([OnePeerContact, TwoPeerContact, ThreePeerContact], ClosestContacts)].
+    [?_assertEqual([?TWO_PEER_1, ?TWO_PEER_2, ?TWO_PEER_3], ClosestContacts)].
 
 should_fill_the_results_with_other_contacts_if_the_closest_bucket_is_not_full({KbucketPid, _}) ->
-    FivePeerContact   = {self(), 2#1111},
-    ThreePeerContact  = {self(), 2#1001},
-    TwoPeerContact    = {self(), 2#1000},
-    put_contacts(KbucketPid, [TwoPeerContact, ThreePeerContact, FivePeerContact]),
+    put_contacts(KbucketPid, [?TWO_PEER_2, ?TWO_PEER_1, ?ONE_PEER_1]),
 
     ClosestContacts = kbucket:closest_contacts(KbucketPid, 2#1010),
 
-    [?_assertEqual([TwoPeerContact, ThreePeerContact, FivePeerContact], ClosestContacts)].
+    [?_assertEqual([?TWO_PEER_2, ?TWO_PEER_1, ?ONE_PEER_1], ClosestContacts)].
 
 should_search_a_key_within_each_bucket_and_refresh_its_content({KbucketPid, Peer}) ->
-    ZeroBucketContactId   = 12,
-    OneBucketContactId    = 15,
-    SecondBucketContactId = 9,
-    ThirdBucketContactId  = 5,
     meck:expect(peer, iterative_find_peers, fun always_successful_iterative_find_peers/2),
 
     kbucket:refresh(KbucketPid),
 
     timer:sleep(50),
-    [?_assertMatch([{_, ZeroBucketContactId}],   kbucket:get(KbucketPid, 0)),
-     ?_assertMatch([{_, OneBucketContactId}],    kbucket:get(KbucketPid, 1)),
-     ?_assertMatch([{_, SecondBucketContactId}], kbucket:get(KbucketPid, 2)),
-     ?_assertMatch([{_, ThirdBucketContactId}],  kbucket:get(KbucketPid, 3))].
+    [?_assertEqual([?ZERO_PEER_1],  kbucket:get(KbucketPid, 0)),
+     ?_assertEqual([?ONE_PEER_1],   kbucket:get(KbucketPid, 1)),
+     ?_assertEqual([?TWO_PEER_1],   kbucket:get(KbucketPid, 2)),
+     ?_assertEqual([?THIRD_PEER_1], kbucket:get(KbucketPid, 3))].
 
 always_successful_iterative_find_peers(_, Key) ->
-    FakeSearchedContact = {self(), Key},
-    [FakeSearchedContact].
+    [?FAKE_PEER(Key)].
 
 put_contacts(KbucketPid, []) ->
     ok;
