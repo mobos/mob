c03292659a403396e4ec5520044231d69d7154ff
JoeBew42 <joebew42@gmail.com>
2017-01-17 19:44:53 +0100

    renaming variables

--- test/bash_t.erl
+++ test/bash_t.erl
@@ -6,13 +6,13 @@
 
 execute_process_with_the_specified_command_test() ->
     FakeFrom = self(),
-    ProcessPid = self(),
+    ExpectedProcessPid = self(),
 
     InitState = #state{params = #{"command" => "a command"}},
 
     meck:new(process),
-    meck:expect(process, exec, fun(_) -> {ok, ProcessPid} end),
+    meck:expect(process, exec, fun(_) -> {ok, ExpectedProcessPid} end),
 
-    {reply, ok, #state{process = Process}} = bash:handle_call(start, FakeFrom, InitState),
+    {reply, ok, #state{process = ProcessPid}} = bash:handle_call(start, FakeFrom, InitState),
 
-    ?assertEqual(Process, ProcessPid).
+    ?assertEqual(ProcessPid, ExpectedProcessPid).
